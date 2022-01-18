f55085a7663c90b78fbcf9258f1777b5968d13c6
inseogi77 <inseogi77@gmail.com>
2022-01-19 00:07:58 +0900

    MVVM APPLY
    MVVM APPLY

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM :INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set 
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("selectedCity");
+            }
+        }
+
+        public City MyProperty
+        {
+            get { return selectedCity; }
+            set { selectedCity = value; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
